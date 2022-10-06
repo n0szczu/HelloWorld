2c19b0fbde1f3318f0e513b65bddbb5b6d363557
n0szczu <patryk.noszczynski@gmail.com>
2022-10-06 16:06:43 +0200

    Commit 2

--- src/pl/vistula/helloworldNoszczyński/HelloWorldNoszczyński.java
+++ src/pl/vistula/helloworldNoszczyński/HelloWorldNoszczyński.java
@@ -3,7 +3,7 @@ package pl.vistula.helloworldNoszczyński;
 public class HelloWorldNoszczyński {
     public static void main(String[] args){
 
-        System.out.println("Hello World");
+        System.out.println("Hello Vistula Noszczyński");
 
     }
 }
