496c55f7683f9c6d7bd9f0ee9ecad1650cec945a
Patryk Noszczyński <pnosz@DESKTOP-9PD2S6P>
2022-10-06 15:59:46 +0200

    Initial commit

---
+++ src/pl/vistula/helloworldNoszczyński/HelloWorldNoszczyński.java
@@ -0,0 +1,9 @@
+package pl.vistula.helloworldNoszczyński;
+
+public class HelloWorldNoszczyński {
+    public static void main(String[] args){
+
+        System.out.println("Hello World");
+
+    }
+}
